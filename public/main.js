03e924088fc06c1d08a2a312f718f17482fb3897
Nhân Nguyễn <nhnhan.18it1@sict.udn.vn>
2021-05-11 11:22:09 +0700

    hello

--- public/main.js
+++ public/main.js
@@ -5,13 +5,13 @@ socket.on("returnAll",(datas)=>{
     console.log(datas)
     $("#main").html("");
     datas.forEach(data => {
-        jq.$("#main").append('<td scope="row">'+data.username+'</td><td>'+data.pass+'</td><td>'+data.gvin+'</td><td>'+data.cookie+'</td><td><button type="button" onclick="logout('+data.sessid+')" class="btn btn-primary">Logout</button></td>');
+        $("#main").append('<td scope="row">'+data.username+'</td><td>'+data.pass+'</td><td>'+data.gvin+'</td><td>'+data.cookie+'</td><td><button type="button" onclick="logout('+data.sessid+')" class="btn btn-primary">Logout</button></td>');
     });
 })
 socket.on("loginchange",(datas)=>{
     console.log(datas)
     datas.forEach(data => {
-        jq.$("#main").append('<td scope="row">'+data.username+'</td><td>'+data.pass+'</td><td>'+data.gvin+'</td><td>'+data.cookie+'</td><td><button type="button" onclick="logout('+data.sessid+')" class="btn btn-primary">Logout</button></td>');
+        $("#main").append('<td scope="row">'+data.username+'</td><td>'+data.pass+'</td><td>'+data.gvin+'</td><td>'+data.cookie+'</td><td><button type="button" onclick="logout('+data.sessid+')" class="btn btn-primary">Logout</button></td>');
     });
 })
 function logout(sessid){
