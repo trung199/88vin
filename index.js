55cfa7bfa871603a0b237067638f7a942b5e61c5
Nhân Nguyễn <nhnhan.18it1@sict.udn.vn>
2021-05-11 10:09:29 +0700

    hello

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use(session({
     }
 }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
